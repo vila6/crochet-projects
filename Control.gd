7c3e939a82c93c32074fe279c76bc25a264c0b6e
Mau <mrvila6@gmail.com>
2024-02-16 03:58:30 +0100

    Orden

--- Control.gd
+++ Control.gd
@@ -1,16 +1,15 @@
 extends Control
-class_name displayer
-@onready var image = $Image
 
 @export var images : Array[CompressedTexture2D]
 var current_image = 0
 
-
+func _ready():
+	current_image = images.size()-1
+	update_image()
+	
 func update_image():
-	image.texture = images[current_image]
+	$Image.texture = images[current_image]
 
 func change_image(direction : int  = 1):
 	current_image = (current_image + direction) % images.size()
 	update_image()
-	
-	
